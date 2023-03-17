0da2a40c462f435a5f3198c5bfd2aea1d5972ff0
MinotourDmitriy <Kkunkka1@mail.ru>
2023-03-17 21:54:12 +0300

    html code add footer.php, changed main.css tags h2,h3

--- _theme/template-parts/footer.php
+++ _theme/template-parts/footer.php
@@ -1,4 +1,53 @@
 <footer>
-    <div class="_container blue_bg"></div>
-    
+    <div class="_container blue_bg">
+        <div class="navigation_footer">
+            <span class="icon_footer"></span>
+            <a href="#" class="nav_footer .block">FAQO</a>
+            <a href="#" class="nav_footer .block">Обучение</a>
+            <a href="#" class="nav_footer .block">Компании</a>
+            <a href="#" class="nav_footer .block">Клиентам</a>
+            <a href="#" class="nav_footer .block">Неисправности</a>
+            <a href="#" class="nav_footer .block">Контакты</a>
+        </div>
+        <section class="general_info">
+            <div class="sqp">
+                <h3>Услуги</h3>
+                <ul>
+                    <li><a href="#">Ремонт бытовой техники</a></li>
+                    <li><a href="#">Ремонт цифровой техники</a></li>
+                    <li><a href="#">Ремонт электрики</a></li>
+                    <li><a href="#">Ремонт сантехники</a></li>
+                    <li><a href="#">Услуги мастера на час</a></li>
+                    <li><a href="#">Транспортировка и установка техники</a></li>
+                    <li><a href="#">Обучение ремонту бытовой техники</a></li>
+                </ul>
+            </div>
+            <div class="sqp">
+                <h3>Вопросы</h3>
+                <ul>
+                    <li><a href="#">Типовые неисправности бытовой техники</a></li>
+                    <li><a href="#">Типовые неисправности холодильников</a></li>
+                    <li><a href="#">Типовые неисправности стиральных машин</a></li>
+                    <li><a href="#">Типовые неисправности кухонных плит</a></li>
+                    <li><a href="#">Типовые неисправности сантехники</a></li>
+                    <li><a href="#">Типовые неисправности электрики</a></li>
+                </ul>
+            </div>
+            <div class="sqp">
+                <h3>Реквизиты</h3>
+                <ul>
+                    <li>ООО "Готово Сервис"</li>
+                    <li>ИНН 9715300665</li>
+                    <li><a href="mailto:iceberg@iceberg.ru">iceberg@iceberg.ru</a></li>
+                    <li><a href="tel:84952283451">8(495)228-34-51</a></li>
+                    <li>127549,г.Москва, Алтуфьевское шоссе, д. 64, офис 10</li>
+                    <li>ОГРН 1177746409720</li>
+                </ul>
+                <a href="#">Политика персональных данных</a>
+                <p>* Некоторые виды работ могут быть выполнены подрядными организациями</p>
+            </div>
+        </section>
+<!--И тут либо <hr> либо div со стилем background-color:blue и height: 3-4px с border-radius -->
+   <p>Вся предоставленная на сайте информация, касающаяся сроков, стоимости и порядка предоставления услуг, носит информационный характер и ни при каких условиях не является публичной офертой, определяемой положениями Статьи 437(2) Гражданского кодекса РФ.</p>     
+    </div>
 </footer>
