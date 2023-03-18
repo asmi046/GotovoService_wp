3b845203879dce340b2266278a0ce30d70749d7a
asmi046 <asmi046@gmail.com>
2023-03-18 15:55:21 +0300

    Сверстанный баннер

--- _theme/index.php
+++ _theme/index.php
@@ -1,7 +1,7 @@
-<?php get_header(); ?>
-    
+<?php get_header(); ?>    
     <?php get_template_part('template-parts/head');?>
     <?php get_template_part('template-parts/main-banner');?>
     <?php get_template_part('template-parts/main-services');?>
+    
     <?php get_template_part('template-parts/footer');?>
 <?php get_footer(); ?>
