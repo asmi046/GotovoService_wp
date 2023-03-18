ef90f12121a6418317c151ba861c6612e5dac7b4
asmi046 <asmi046@gmail.com>
2023-03-18 16:22:35 +0300

    выровненные услуги на главной

--- _theme/index.php
+++ _theme/index.php
@@ -2,6 +2,5 @@
     <?php get_template_part('template-parts/head');?>
     <?php get_template_part('template-parts/main-banner');?>
     <?php get_template_part('template-parts/main-services');?>
-    
     <?php get_template_part('template-parts/footer');?>
 <?php get_footer(); ?>
--- _theme/template-parts/main-services.php
+++ _theme/template-parts/main-services.php
@@ -1,13 +1,14 @@
-<section class="main_services">
+<section class="main_services m_b_40">
         <div class="_container">
-            <div class="usl_line usl_line_top">
-                <div class="usl_blk gray_bg">
-                    <h3>Ремонт Apple</h3>
+            <h2>Услуги</h2>
+            <div class="usl_line usl_line_top d_flex">
+                <div class="usl_blk gray_bg f_2 d_flex f_col">
+                    <h3>Ремонт <br/>Apple</h3>
                     <span class="price">от 500 р.</span>
                 </div>
 
-                <div class="usl_blk blue_bg">
-                    <h3>РЕМОНТ КОМПЬЮТЕРОВ</h3>
+                <div class="usl_blk blue_bg f_4 m_lr_25 white_color d_flex f_col">
+                    <h3>РЕМОНТ <br/>КОМПЬЮТЕРОВ</h3>
                     <ul>
                         <li>Бесплатная диагностика</li>
                         <li>Настройка и Чистка компьютера</li>
@@ -16,15 +17,39 @@
                     <span class="price">от 500 р.</span>
                 </div>
 
-                <div class="usl_blk gray_bg">
-                    <h3>Ремонт бытовой техники</h3>
+                <div class="usl_blk gray_bg f_2 d_flex f_col">
+                    <h3>Ремонт <br/>бытовой <br/>техники</h3>
                     <span class="price">от 500 р.</span>
                 </div>
 
             </div>
 
-            <div class="usl_line usl_line_bottom">
+            <div class="usl_line usl_line_bottom d_flex">
+                <div class="usl_blk blue_bg white_color f_2 d_flex f_col">
+                    <h3>ДАННЫЕ И <br/>ФАЙЛЫ</h3>
+                    <ul>
+                        <li>Установка</li>
+                        <li>Переустановка</li>
+                        <li>Восстановление</li>
+                    </ul>
+                    <span class="price">от 500 р.</span>
+                </div>
                 
+                <div class="usl_blk gray_bg f_2 m_lr_25 d_flex f_col">
+                    <h3>Ремонт <br/>оргтехники</h3>
+                    <span class="price">от 500 р.</span>
+                </div>
+
+                <div class="usl_blk gray_bg f_4 d_flex f_col">
+                    <h3>Ремонт <br/>ноутбуков</h3>
+                    <ul>
+                        <li>Бесплатная диагностика</li>
+                        <li>Настройка и Чистка ноутбука</li>
+                        <li>Модернизация ноутбука</li>
+                    </ul>
+                    <span class="price">от 500 р.</span>
+                </div>
+
             </div>
         </div>
 </section>
