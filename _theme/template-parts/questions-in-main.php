d88742bc60f29b9b0def589ddb3cb076fe0c7e7d
MinotourDmitriy <Kkunkka1@mail.ru>
2023-03-19 15:18:05 +0300

    add file questions.css, activated css file in function.php, add code questions-in-main.php

---
+++ _theme/template-parts/questions-in-main.php
@@ -0,0 +1,32 @@
+<section class="free_question">
+    <div class="_container blue_bg white_color brad_12 m_b_40 pad_50_40">
+        <div class="d_flex m_b_40">
+        <div class="f_1">
+                <h2>Остались вопросы?</h2>
+                <p>Специалист<br>
+                с вами свяжется
+                </p>      
+        </div>
+            <div class="f_1">
+                    <form action="#" class="d_flex">
+                        <div class="d_flex f_1 f_col m_r_70">
+                        <input type="text" name="name" placeholder="Имя*">
+                        <input type="tel" name="phone" placeholder="+7 (___) ___-__-___">
+                        </div>
+                        <div class="d_flex f_1 f_col m_r_70">
+                            <textarea class="textarea f_1" name="" placeholder="Вопрос"></textarea>
+                        </div>
+                    </form>
+            </div>
+        </div>
+        <div class="d_flex">
+            <div class="f_1 d_flex">
+                <p>* Отправляя заявку, вы соглашаетесь на обработку персональных данных</p>
+            </div>
+            <div class="f_1 d_flex">
+                <button class="btn m_t_a">Отправить</button>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
