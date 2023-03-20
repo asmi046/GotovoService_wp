bf6d6de5ad1d84c8b20587d82cb01c9180a0484e
MinotourDmitriy <Kkunkka1@mail.ru>
2023-03-20 22:39:12 +0300

    add new file cooperation.css in add code in function.php, add new code cooperation-in-main.php

---
+++ _theme/template-parts/cooperation-in-main.php
@@ -0,0 +1,28 @@
+<section class="main-cooperation">
+<div class="_container blue_bg white_color brad_12 m_b_40 pad_50_40 d_flex">
+    <div class="f_1">
+        <h2>Порядок<br>сотрудничества</h2>
+        <button class="btn m_t_22">Записаться</button>
+    </div>
+    <div class="f_1">
+        <div class="d_flex jc_sb">
+            <div class="f_col d_flex r_gap">
+                <p class="cooperation-number number-line">1</p>
+                <p">Минута требуется<br>для оформления<br>заявки</p>
+            </div>
+            <div class="f_col d_flex r_gap">
+                <p class="cooperation-number number-line">5</p>
+                <p>Минут нужно<br>для выезда<br>мастера</p>
+            </div>
+            <div class="f_col d_flex r_gap">
+                <p class="cooperation-number number-line">60</p>
+                <p>Минут нужно<br>мастеру, чтобы<br>добраться до вас</p>
+            </div>
+            <div class="f_col d_flex r_gap">
+                <p class="cooperation-number number-line">30</p>
+                <p>Минут в среднем<br>требуется<br>на ремонт</p>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
