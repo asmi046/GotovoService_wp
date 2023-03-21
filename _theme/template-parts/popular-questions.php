d27fcb5edbfc5ecb3b3e7b1191aad895352b28f9
asmi046 <asmi046@gmail.com>
2023-03-22 02:32:49 +0300

    Частые вопросы и цены

---
+++ _theme/template-parts/popular-questions.php
@@ -0,0 +1,43 @@
+<section class="popular-questions m_b_40">
+    <div class="_container">
+        <h2>Популярные вопросы</h2>
+        <div class="d_flex">   
+            <div class="f_1 faq">
+                <details>
+                    <summary>Почему холодильник перестал охлаждать?</summary>
+                    <div class="response">
+                        Something small enough to escape casual notice.
+                    </div>
+                </details>
+
+                <details>
+                    <summary>Из-за чего духовка показывает неправильную температуру?</summary>
+                    <div class="response">
+                        Something small enough to escape casual notice.
+                    </div>
+                </details>
+
+                <details>
+                    <summary>По какой причине кофемашина не реагирует на включение?</summary>
+                    <div class="response">
+                        Something small enough to escape casual notice.
+                    </div>
+                </details>
+
+                <details>
+                    <summary>Пылесос перестал включаться, что могло случиться?</summary>
+                    <div class="response">
+                        Something small enough to escape casual notice.
+                    </div>
+                </details>
+
+                <a href="#" class="btn btn_full">Получить консультацию</a>
+            </div>
+
+            <div class="f_1 d_flex pos_rel">
+                <div class="circle gray_bg pos_abs"></div>
+                <img class="m_auto" src="<?php echo get_template_directory_uri();?>/img/questionsman.webp" alt="">  
+            </div>
+        </div>
+    </div>
+</section>
---
+++ _theme/template-parts/prices.php
@@ -0,0 +1,15 @@
+<section class="prices">
+    <div class="_container">
+        <h2>Цены на услуги</h2>
+        <div class="d_flex f_col">
+            <div class="selector d_flex">
+
+            </div>
+
+            <div class="services">
+
+            </div>
+
+        </div>
+    </div>
+</section>
