1d2fd75d841e3c5d9b62b231b91b65cd3f1b86a4
MinotourDmitriy <Kkunkka1@mail.ru>
2023-03-28 00:40:44 +0300

    portfolio.php

---
+++ _theme/template-parts/portfolio.php
@@ -0,0 +1,15 @@
+<section class="main__portfolio">
+    <div class="_container gray_bg brad_12">
+        <h2>Портфолио</h2>
+            <div class="d_flex f_col">
+                <div class="m_b_22">
+                    <div class="d_flex jc_sb">
+                            <a class="white_bg  brad_12 select_btn" href="#">Ремонт ноутбуков</a>
+                            <a class="white_bg  brad_12 select_btn active" href="#">Ремонт компьютеров</a>
+                            <a class="white_bg  brad_12 select_btn" href="#">Ремонт телефонов</a>
+                            <a class="white_bg  brad_12 select_btn" href="#">Ремонт холодильников</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+</section>
