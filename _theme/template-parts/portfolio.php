028ad8f9072336ee94b7931a67610843f77fb2fe
MinotourDmitriy <Kkunkka1@mail.ru>
2023-03-28 01:50:54 +0300

    add red_time.svg, add code portfolio.php not mobile code it's next my work

--- _theme/template-parts/portfolio.php
+++ _theme/template-parts/portfolio.php
@@ -1,13 +1,48 @@
 <section class="main__portfolio">
-    <div class="_container gray_bg brad_12">
+    <div class="_container gray_bg brad_12 m_b_40">
         <h2>Портфолио</h2>
-            <div class="d_flex f_col">
+            <div class="d_flex f_col m_b_40">
                 <div class="m_b_22">
                     <div class="d_flex jc_sb">
                             <a class="white_bg  brad_12 select_btn" href="#">Ремонт ноутбуков</a>
                             <a class="white_bg  brad_12 select_btn active" href="#">Ремонт компьютеров</a>
                             <a class="white_bg  brad_12 select_btn" href="#">Ремонт телефонов</a>
                             <a class="white_bg  brad_12 select_btn" href="#">Ремонт холодильников</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="portfolio-application d_flex jc_sb">
+                <div class="application d_flex f_col">
+                    <img src="<?php echo get_template_directory_uri();?>/img/picture/zayavka1.webp" alt="Заявка1">
+                    <div class="application-info">
+                    <p class="application-identifier">Заявка 358756</p>
+                    <p class="application-cause">Причина: Утечка хладогента</p>
+                    <p class="application-made">Что было сделано: Заправка фреоном</p>
+                    <p class="application-time pos_rel">Время: 45 минут</p>
+                    <p class="application-price pos_rel">Цена: <span>7650 Р</span></p>
+                    </div>
+                </div>
+
+                <div class="application d_flex f_col">
+                    <img src="<?php echo get_template_directory_uri();?>/img/picture/zayavka1.webp" alt="Заявка1">
+                    <div class="application-info">
+                    <p class="application-identifier">Заявка 358756</p>
+                    <p class="application-cause">Причина: Утечка хладогента</p>
+                    <p class="application-made">Что было сделано: Заправка фреоном</p>
+                    <p class="application-time pos_rel">Время: 45 минут</p>
+                    <p class="application-price pos_rel">Цена: <span>7650 Р</span></p>
+                    </div>
+                </div>
+
+                <div class="application d_flex f_col">
+                    <img src="<?php echo get_template_directory_uri();?>/img/picture/zayavka1.webp" alt="Заявка1">
+                    <div class="application-info">
+                    <p class="application-identifier">Заявка 358756</p>
+                    <p class="application-cause">Причина: Утечка хладогента</p>
+                    <p class="application-made">Что было сделано: Заправка фреоном</p>
+                    <p class="application-time pos_rel">Время: 45 минут</p>
+                    <p class="application-price pos_rel">Цена: <span>7650 Р</span></p>
                     </div>
                 </div>
             </div>
