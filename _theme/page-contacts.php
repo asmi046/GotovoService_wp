8498a712d6db253f36e83f9621f38ed7056ec4b1
asmi046 <asmi046@gmail.com>
2023-03-30 12:24:48 +0300

    страница контактов

---
+++ _theme/page-contacts.php
@@ -0,0 +1,16 @@
+
+<?php 
+/*
+Template Name: Контакты
+Template Post Type: page
+*/
+
+get_header(); ?>
+
+<?php get_template_part('template-parts/head');?>
+
+<?php get_template_part('template-parts/page-head');?>
+
+<?php get_template_part('template-parts/footer');?>
+
+<?php get_footer(); ?>
---
+++ _theme/template-parts/page-head.php
@@ -0,0 +1,6 @@
+
+<section class="page_head">
+    <div class="_container">
+        <?php get_template_part('template-parts/banner-menu');?>
+    </div>
+</section>
