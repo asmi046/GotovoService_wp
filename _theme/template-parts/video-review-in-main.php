0161cd8f1f3a7b430ed709d9bd9af75177e8f357
MinotourDmitriy <Kkunkka1@mail.ru>
2023-03-31 02:20:28 +0300

    add new block review-video

---
+++ _theme/template-parts/video-review-in-main.php
@@ -0,0 +1,103 @@
+<section class="main-video">
+    <div class="_container">
+        <h2>Видео отзывы</h2>
+        <div class="video__reviews-link d_flex jc_sb">
+            <div class="card-video">
+                <div class="card-video__img brad_12">
+                    <img src="<?php echo get_template_directory_uri();?>/img/picture/review1.webp" sizes="16x16" alt="">
+                </div>
+                <div class="card-video__ifor-block white_color">
+                    <div class="card-video__play">
+                        <div class="play-button"></div>
+                    </div>
+                    <div class="card-video__text">
+                    <p class="video__text-name m_b_22">
+                    Ирина
+                    </p>
+
+                    <p class="video__text-review">
+                    Отзыв на ремонт компьютера
+                    </p>
+
+                    <p class="video__text-applic">
+                    Заявка <span>458755</span>
+                    </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-video">
+                <div class="card-video__img brad_12">
+                    <img src="<?php echo get_template_directory_uri();?>/img/picture/review2.webp" sizes="16x16" alt="">
+                </div>
+                <div class="card-video__ifor-block white_color">
+                    <div class="card-video__play">
+                        <div class="play-button"></div>
+                    </div>
+                    <div class="card-video__text">
+                    <p class="video__text-name m_b_22">
+                    Ирина
+                    </p>
+
+                    <p class="video__text-review">
+                    Отзыв на ремонт компьютера
+                    </p>
+
+                    <p class="video__text-applic">
+                    Заявка <span>458755</span>
+                    </p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card-video">
+                <div class="card-video__img brad_12">
+                    <img src="<?php echo get_template_directory_uri();?>/img/picture/review3.webp" sizes="16x16" alt="">
+                </div>
+                <div class="card-video__ifor-block white_color">
+                    <div class="card-video__play">
+                        <div class="play-button"></div>
+                    </div>
+                    <div class="card-video__text">
+                    <p class="video__text-name m_b_22">
+                    Ирина
+                    </p>
+
+                    <p class="video__text-review">
+                    Отзыв на ремонт компьютера
+                    </p>
+
+                    <p class="video__text-applic">
+                    Заявка <span>458755</span>
+                    </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-video">
+                <div class="card-video__img brad_12">
+                    <img src="<?php echo get_template_directory_uri();?>/img/picture/review4.webp" sizes="16x16" alt="">
+                </div>
+                <div class="card-video__ifor-block white_color">
+                    <div class="card-video__play">
+                        <div class="play-button"></div>
+                    </div>
+                    <div class="card-video__text">
+                    <p class="video__text-name m_b_22">
+                    Ирина
+                    </p>
+
+                    <p class="video__text-review">
+                    Отзыв на ремонт компьютера
+                    </p>
+
+                    <p class="video__text-applic">
+                    Заявка <span>458755</span>
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
