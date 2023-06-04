4e6fb69d1a407ba828bd3dcf354047b1cb6105ae
asmi046 <asmi046@gmail.com>
2023-06-04 11:14:38 +0300

    Форма вызова оптимизированная

--- _theme/template-parts/to-home.php
+++ _theme/template-parts/to-home.php
@@ -1,5 +1,65 @@
 <section class="to_home_form">
     <div class="_container">
         <h2>Срочный вызов мастера на дом</h2>
+        
+        <form action="">
+            <div class="form_to_home">
+                <div class="left">
+                    <p>Выберите дату</p>
+                    <input name="date" type="date" placeholder="Выберите дату">
+
+                    <p>Выберите время</p>
+                    <div class="radiogroup">
+                        
+                        <label for="t8_10">
+                            <input id="t8_10" value="08:00-10:00" name="time" type="radio">
+                            <span>08:00-10:00</span>
+                        </label>
+
+                        <label for="t10_12">
+                            <input id="t10_12" value="10:00-12:00" name="time" type="radio">
+                            <span>10:00-12:00</span>
+                        </label>
+
+                        <label for="t12_14">
+                            <input id="t12_14" value="12:00-14:00" name="time" type="radio">
+                            <span>12:00-14:00</span>
+                        </label>
+
+                        <label for="t14_16">
+                            <input id="t14_16" value="14:00-16:00" name="time" type="radio">
+                            <span>14:00-16:00</span>
+                        </label>
+
+                        <label for="t16_18">
+                            <input id="t16_18" value="16:00-18:00" name="time" type="radio">
+                            <span>16:00-18:00</span>
+                        </label>
+                        
+                        <label for="t18_20">
+                            <input id="t18_20" value="18:00-20:00" name="time" type="radio">
+                            <span>18:00-20:00</span>
+                        </label>
+
+                        <p class="summery">Внимание: Реальная дата и время выезда могут отличаться от выбранных из-за загруженности мастеров. Точную дату и время вам сообщит менеджер.</p>
+                    </div>
+                </div>
+                
+                <div class="right">
+                    <p>Контактные данные</p>
+                    <input type="text" name="name" placeholder="Имя*">
+                    <input type="tel" name="phone" placeholder="Телефон*">
+                    <p>Адрес</p>
+                    <input type="text" name="adress" placeholder="Адрес">
+                    <button class="btn">Отправить</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+
+<section class="green_line">
+    <div class="_container">
+        <p>Вы заботитесь об окружающей среде, выбирая ремонт, а не покупку новой техники.</p>
     </div>
 </section>
